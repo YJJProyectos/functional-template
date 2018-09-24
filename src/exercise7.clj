fb7ec76ec421c3856975e548c03c37ff38cd6903
Jia Jun Yang <jyang@baufest.com>
2018-09-24 00:34:24 -0300

    test 7 multi method

--- src/exercise7.clj
+++ src/exercise7.clj
@@ -1,5 +1,5 @@
 (ns exercise7)
-(defn fmap
+(comment defn fmap
   [x y]
   ( if ( map? y )
     (apply merge
@@ -13,3 +13,12 @@
       )
     )
   )
+
+(defmulti fmap (fn [x y]  (type y) ) )
+(defmethod fmap clojure.lang.PersistentList [x y] (map x y) )
+(defmethod fmap clojure.lang.PersistentVector [x y] (into [] (map x y) ))
+(defmethod fmap clojure.lang.PersistentArrayMap [x y]
+  (apply merge
+         (map (fn [[k v]] {k (x v) }) y))
+  )
+(defmethod fmap :default [x y] x)
